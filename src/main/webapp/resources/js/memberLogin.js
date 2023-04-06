e24e630e05aa1147cee23f51056fbaac263e37b7
wuxinanzi <suu308@gmail.com>
2023-04-06 17:52:05 +0900

    COMCOM

--- src/main/webapp/resources/js/memberLogin.js
+++ src/main/webapp/resources/js/memberLogin.js
@@ -1,24 +1,22 @@
-$(document).ready(function() {
-    $("#memberLogin").on("click", function() {
-      let id = $(this).attr("data-member-id");
-    //   let pw = $(this).attr("data-member-pw");
-  
-      $.ajax({
-        url: "/member/memberLogin",
-        type: "POST",
-        data: {
-          id: id,
-          
-        },
-  
-        success: function(result) {
-        
-          if (result.success) { // 로그인 성공 여부 판단
-            alert("로그인 성공했습니다.")
-          } else {
-            alert("로그인 실패했습니다.");
-          }
-        }
-      });
-    });
+$("#memberLogin").on("click", function() {
+  let id = $(this).attr("data-member-id");
+
+
+  $.ajax({
+    url: "/member/memberLogin",
+    type: "POST",
+    data: {
+      id: id,
+      
+    },
+
+    success: function(result) {
+    
+      if (result.success) { // 로그인 성공 여부 판단
+        alert("로그인 성공했습니다.")
+      } else {
+        alert("로그인 실패했습니다.");
+      }
+    }
   });
+});
